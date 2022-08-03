4821c8824797cc436d48323cf0282a51eab77d4e
MareMare <MareMare@users.noreply.github.com>
2022-08-03 15:46:35 +0900

    dev (#1)
    * Create CODEOWNERS
    
    * Create dependabot.yml
    
    * `IpAddressMonitor` プロジェクトを追加します。
    
    * ソリューション項目を追加します。
    
    * `.vscode` フォルダを追加します。
    
    * Create ci.yml
    
    * Improve `SA1117`.

---
+++ src/IpAddressMonitor.UnitTests/NetIpInfoUnitTest.cs
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NetIpInfoUnitTest.cs" company="MareMare">
+// Copyright © 2022 MareMare. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace IpAddressMonitor.UnitTests;
+
+public class NetIpInfoUnitTest
+{
+    [Fact]
+    public void NetIpInfo_GetNetIpInfos_Test()
+    {
+        var actual = NetIpInfo.GetNetIpInfos().ToArray();
+        Assert.NotNull(actual);
+        Assert.True(actual.Any());
+    }
+}
